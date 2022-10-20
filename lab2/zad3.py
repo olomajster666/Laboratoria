3c7cbfd26720da085ade7a1c348c5dc55c1a1c22
olomajster666 <soda0001@o2.pl>
2022-10-20 18:29:14 +0200

    zadania lab 2

--- lab2/zad3.py
+++ lab2/zad3.py
@@ -1,8 +1,17 @@
-o = int(input("Jaką operację chcesz wykonać? 1)dodawanie 2)odejmowanie 3)mnożenie 4)dzielenie 5)potęgowanie : "))
-x = int(input("podaj pierwszy argument: "))
-y = int(input("podaj drugi argument: "))
+o = int(input('''Jaką operację chcesz wykonać? 
+1)dodawanie 
+2)odejmowanie 
+3)mnożenie 
+4)dzielenie 
+5)potęgowanie
+'''))
+x = float(input("podaj pierwszy argument: "))
+y = float(input("podaj drugi argument: "))
 
-if o == 1:
+if o > 5 or o < 0:
+    print("błędny argumant operacji")
+    exit()
+elif o == 1:
     print("wynik: ",x+y)
 elif o == 2:
     print("wynik: ",x-y)
@@ -12,5 +21,4 @@ elif o == 4:
     print("wynik: ", x/y)
 elif o == 5:
     print("wynik: ", x**y)
-else:
-    print("błędny argument operacji")
+
