76dccd878832d3965b70d0178f0f23c19df39f64
olomajster666 <116032430+olomajster666@users.noreply.github.com>
2022-11-03 17:16:57 +0100

    Delete zad5.py

--- zad5.py
+++
@@ -1,9 +0,0 @@
-n = int(input("podaj liczbe studentow: "))
-x = 1
-s = 0
-while(n>=x):
-    p=int(input("podaj liczbe punktow studenta: "))
-    x=x+1
-    s =s+p
-
-print("srednia punktow wynosi:", s/n )
